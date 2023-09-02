7bb481b09af75b04ba09c152faf93235254b65cd
Preisler <preisler.andras@gmail.com>
2023-09-02 13:14:59 +0200

    main

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import express from 'express';
-import dotenv from 'dotenv';
+const express = require('express');
+const dotenv = require('dotenv');
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.get('/', (req, res) => {
-  res.send('Express + TypeScript Server');
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
