48d4770c8f48a653caeafa74a12b0660b1de7c98
Preisler <preisler.andras@gmail.com>
2023-09-02 17:44:59 +0200

    main

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,2 @@
-const express = require('express');
-const dotenv = require('dotenv');
+import express from 'express';
 
-dotenv.config();
-
-const app = express();
-const port = process.env.PORT;
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
-
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
