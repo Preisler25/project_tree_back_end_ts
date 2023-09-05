3456682587156b5af1f033d80770d71352b220fa
Preisler <preisler.andras@gmail.com>
2023-09-05 10:51:56 +0200

    main

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/example', (req: Request, res: Response) => {
+  res.send('Hello, Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
